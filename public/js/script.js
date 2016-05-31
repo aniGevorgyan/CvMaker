d6497f050aafb744bf5d1d1f49e13ac5f059d45a
Gurgen <gurgen.gharibyan@gmail.com>
2016-05-31 12:27:12 +0400

    commit_js

---
+++ public/js/script.js
@@ -0,0 +1,5 @@
+$( document ).ready(function() {
+    var page_height = $( window ).height();
+    $('.content_1').css('min-height', (page_height-117)+'px');
+    $('.content_login').css('min-height', (page_height-117)+'px');
+});
