6045e851abe1e5fc4ae4e5abd62a83d6fe24e3ef
Gurgen <gurgen.gharibyan@gmail.com>
2016-06-08 17:30:41 +0400

    free_preview_commit

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID': '219299121795972',
 		'clientSecret': '9bae48714c92cd40ef8535e883291118',
-		'callbackURL': 'http://localhost:5555/auth/facebook/callback'
+		'callbackURL': 'http://localhost:8080/auth/facebook/callback'
 	},
 
 	'googleAuth' : {
--- public/js/script.js
+++ public/js/script.js
@@ -2,4 +2,9 @@ $( document ).ready(function() {
     var page_height = $( window ).height();
     $('.content_1').css('min-height', (page_height-117)+'px');
     $('.content_login').css('min-height', (page_height-117)+'px');
+
+
+    $('#preview').click(function () {
+        $('.div').show();
+    })
 });
