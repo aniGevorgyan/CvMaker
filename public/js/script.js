3b349d70a7a568ded49f61bf9ddc5f4273b7fd3b
aniGevorgyan <anigevorgyan90@gmail.com>
2016-06-09 02:13:00 +0300

    download_commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,35 @@
-$( document ).ready(function() {
-    var page_height = $( window ).height();
-    $('.content_1').css('min-height', (page_height-117)+'px');
-    $('.content_login').css('min-height', (page_height-117)+'px');
+$(document).ready(function () {
+    var page_height = $(window).height();
+    $('.content_1').css('min-height', (page_height - 117) + 'px');
+    $('.content_login').css('min-height', (page_height - 117) + 'px');
 
+    function infoFromInputs(infoFromTo) {
+        var input = $("input[name=" + infoFromTo + "]").val();
+        $("." + infoFromTo + "").text(input);
+    }
 
     $('#preview').click(function () {
         $('.div').show();
-    })
+        infoFromInputs("firstName");
+        infoFromInputs("lastName");
+        infoFromInputs("dob");
+    });
+    $('.close_button').click(function () {
+        $('.div').hide();
+    });
+
+    var doc = new jsPDF();
+    var specialElementHandlers = {
+        '#editor': function (element, renderer) {
+            return true;
+        }
+    };
+
+    $('#cmd').click(function () {
+        doc.fromHTML($('body').html(), 15, 15, {
+            'width': 170,
+            'elementHandlers': specialElementHandlers
+        });
+        doc.save('sample-file.pdf');
+    });
 });
