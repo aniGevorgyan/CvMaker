01e9e41a4d0470cf5aa0a7de7929c5a1f893a3fb
aniGevorgyan <anigevorgyan90@gmail.com>
2016-06-20 13:27:03 +0300

    profile_page_commit

--- public/js/pdfFromHTML.js
+++ public/js/pdfFromHTML.js
@@ -25,7 +25,7 @@ function pdfToHTML() {
         function (dispose) {
             // dispose: object with X, Y of the last line add to the PDF
             //          this allow the insertion of new lines after html
-            pdf.save('html2pdf.pdf');
+            pdf.save('armCv.pdf');
         }
     )
 }
